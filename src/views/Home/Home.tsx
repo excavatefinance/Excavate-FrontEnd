e556dd397b7c680a42dc9c9c37aa15a57f2b5a8d
excavatefinance <81165639+excavatefinance@users.noreply.github.com>
2021-04-07 12:26:11 +0100

    Language changes

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -62,7 +62,7 @@ const Home: React.FC = () => {
         <Heading as="h1" size="xl" mb="24px" color="secondary">
           {TranslateString(576, 'Excavate Finance')}
         </Heading>
-        <Text>{TranslateString(578, 'Top 3 best DEFI app on Binance Smart Chain.')}</Text>
+        <Text>{TranslateString(578, 'The all in one DeFi app on the Binance Smart Chain.')}</Text>
       </Hero>
       <div>
         <Cards>
