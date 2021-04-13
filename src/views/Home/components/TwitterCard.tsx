bb4da8bf5a298834d4cff6a36af91d56a8cdecb8
excavatefinance <81165639+excavatefinance@users.noreply.github.com>
2021-04-13 15:17:10 +0100

    Update TwitterCard.tsx

--- src/views/Home/components/TwitterCard.tsx
+++ src/views/Home/components/TwitterCard.tsx
@@ -35,7 +35,7 @@ const TwitterCard = () => {
         <Timeline
           dataSource={{
             sourceType: 'profile',
-            screenName: 'GooseFinance'
+            screenName: 'RevealFinance'
           }}
           options={{
             height: '300',
