99e9199067f25cbb387594d75722bdf943a7d352
excavatefinance <81165639+excavatefinance@users.noreply.github.com>
2021-04-26 16:33:12 +0100

    Update Farms.tsx

--- src/views/Farms/Farms.tsx
+++ src/views/Farms/Farms.tsx
@@ -1,125 +1,34 @@
 import React, { useEffect, useCallback, useState } from 'react'
-import { Route, useRouteMatch, useLocation } from 'react-router-dom'
+import { Route, useRouteMatch } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import BigNumber from 'bignumber.js'
-import { useWeb3React } from '@web3-react/core'
-import { Image, Heading, RowType, Toggle, Text } from 'voidfarm-toolkit'
-import styled from 'styled-components'
+import { useWallet } from '@binance-chain/bsc-use-wallet'
+import { provider } from 'web3-core'
+import { Image, Heading } from '@pancakeswap-libs/uikit'
+import { BLOCKS_PER_YEAR, CAKE_PER_BLOCK, CAKE_POOL_PID } from 'config'
 import FlexLayout from 'components/layout/Flex'
 import Page from 'components/layout/Page'
-import { useFarms, usePriceCakeBusd, useGetApiPrices } from 'state/hooks'
+import { useFarms, usePriceBnbBusd, usePriceCakeBusd } from 'state/hooks'
 import useRefresh from 'hooks/useRefresh'
 import { fetchFarmUserDataAsync } from 'state/actions'
-import { Farm } from 'state/types'
+import { QuoteToken } from 'config/constants/types'
 import useI18n from 'hooks/useI18n'
-import { getBalanceNumber } from 'utils/formatBalance'
-import { getFarmApy } from 'utils/apy'
-import { orderBy } from 'lodash'
-
 import FarmCard, { FarmWithStakedValue } from './components/FarmCard/FarmCard'
-import Table from './components/FarmTable/FarmTable'
 import FarmTabButtons from './components/FarmTabButtons'
-import SearchInput from './components/SearchInput'
-import { RowProps } from './components/FarmTable/Row'
-import ToggleView from './components/ToggleView/ToggleView'
-import { DesktopColumnSchema, ViewMode } from './components/types'
-import Select, { OptionProps } from './components/Select/Select'
+import Divider from './components/Divider'
 
-const ControlContainer = styled.div`
-  display: flex;
-  width: 100%;
-  align-items: center;
-  position: relative;
-  justify-content: space-between;
-  flex-direction: column;
-  ${({ theme }) => theme.mediaQueries.sm} {
-    flex-direction: row;
-    flex-wrap: wrap;
-    padding: 16px 32px;
-  }
-`
+export interface FarmsProps{
+  tokenMode?: boolean
+}
 
-const StyledPage = styled(Page)`
-  background-repeat: no-repeat;
-  background-position: top;
-  background-size:contain;
-  background-image: url("/images/midVoid.png");
-`
-
-const ToggleWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  margin-left: 10px;
-  ${Text} {
-    margin-left: 8px;
-  }
-`
-
-const LabelWrapper = styled.div`
-  > ${Text} {
-    font-size: 12px;
-  }
-`
-
-const FilterContainer = styled.div`
-  display: flex;
-  align-items: center;
-  width: 100%;
-  padding: 8px 0px;
-  ${({ theme }) => theme.mediaQueries.sm} {
-    width: auto;
-    padding: 0;
-  }
-`
-
-const ViewControls = styled.div`
-  flex-wrap: wrap;
-  justify-content: space-between;
-  display: flex;
-  align-items: center;
-  width: 100%;
-  > div {
-    padding: 8px 0px;
-  }
-  ${({ theme }) => theme.mediaQueries.sm} {
-    justify-content: flex-start;
-    width: auto;
-    > div {
-      padding: 0;
-    }
-  }
-`
-
-const StyledImage = styled(Image)`
-  margin-left: auto;
-  margin-right: auto;
-  margin-top: 75px;
-  margin-bottom: -30px;
-`
-
-const Header = styled.div`
-  padding: 32px 0px;
-  background: ${({ theme }) => theme.colors.gradients.bubblegum};
-  padding-left: 16px;
-  padding-right: 16px;
-  ${({ theme }) => theme.mediaQueries.sm} {
-    padding-left: 24px;
-    padding-right: 24px;
-  }
-`
-
-const Farms: React.FC = () => {
+const Farms: React.FC<FarmsProps> = (farmsProps) => {
   const { path } = useRouteMatch()
-  const { pathname } = useLocation()
   const TranslateString = useI18n()
   const farmsLP = useFarms()
   const cakePrice = usePriceCakeBusd()
-  const [query, setQuery] = useState('')
-  const [viewMode, setViewMode] = useState(ViewMode.TABLE)
-  const { account } = useWeb3React()
-  const [sortOption, setSortOption] = useState('hot')
-  const prices = useGetApiPrices()
-
+  const bnbPrice = usePriceBnbBusd()
+  const { account, ethereum }: { account: string; ethereum: provider } = useWallet()
+  const {tokenMode} = farmsProps;
 
   const dispatch = useDispatch()
   const { fastRefresh } = useRefresh()
@@ -129,237 +38,84 @@ const Farms: React.FC = () => {
     }
   }, [account, dispatch, fastRefresh])
 
-  const [stackedOnly, setStackedOnly] = useState(false)
-  const isActive = !pathname.includes('history')
-  const isPool = pathname.includes('pools')
+  const [stakedOnly, setStakedOnly] = useState(false)
 
-  let activeFarms;
-  let inactiveFarms;
+  const activeFarms = farmsLP.filter((farm) => !!farm.isTokenOnly === !!tokenMode && farm.multiplier !== '0X')
+  const inactiveFarms = farmsLP.filter((farm) => !!farm.isTokenOnly === !!tokenMode && farm.multiplier === '0X')
 
-  if (isPool){
-    activeFarms = farmsLP.filter((farm) => farm.isTokenOnly && farm.multiplier !== '0X')
-    inactiveFarms = farmsLP.filter((farm) => farm.isTokenOnly && farm.multiplier === '0X')
-  }
-  else{
-    activeFarms = farmsLP.filter((farm) => !farm.isTokenOnly && farm.multiplier !== '0X')
-    inactiveFarms = farmsLP.filter((farm) => !farm.isTokenOnly && farm.multiplier === '0X')
-  }
-
-  const stackedOnlyFarms = activeFarms.filter(
+  const stakedOnlyFarms = activeFarms.filter(
     (farm) => farm.userData && new BigNumber(farm.userData.stakedBalance).isGreaterThan(0),
   )
 
-  const stackedInactiveFarms = inactiveFarms.filter(
-    (farm) => farm.userData && new BigNumber(farm.userData.stakedBalance).isGreaterThan(0),
-  )
-
-  const sortFarms = (farms: FarmWithStakedValue[]): FarmWithStakedValue[] => {
-    switch (sortOption) {
-      case 'apr':
-        return orderBy(farms, (farm: FarmWithStakedValue) => farm.apy, 'desc')
-      case 'multiplier':
-        return orderBy(
-          farms,
-          (farm: FarmWithStakedValue) => (farm.multiplier ? Number(farm.multiplier.slice(0, -1)) : 0),
-          'desc',
-        )
-      case 'earned':
-        return orderBy(farms, (farm: FarmWithStakedValue) => (farm.userData ? farm.userData.earnings : 0), 'desc')
-      case 'liquidity':
-        return orderBy(farms, (farm: FarmWithStakedValue) => Number(farm.liquidity), 'desc')
-      default:
-        return farms
-    }
-  }
-  
+  // /!\ This function will be removed soon
+  // This function compute the APY for each farm and will be replaced when we have a reliable API
+  // to retrieve assets prices against USD
   const farmsList = useCallback(
-    (farmsToDisplay: Farm[]): FarmWithStakedValue[] => {
-      let farmsToDisplayWithAPY: FarmWithStakedValue[] = farmsToDisplay.map((farm) => {
-        if (!farm.lpTotalInQuoteToken || !prices) {
-          return farm
+    (farmsToDisplay, removed: boolean) => {
+      // const cakePriceVsBNB = new BigNumber(farmsLP.find((farm) => farm.pid === CAKE_POOL_PID)?.tokenPriceVsQuote || 0)
+      const farmsToDisplayWithAPY: FarmWithStakedValue[] = farmsToDisplay.map((farm) => {
+        // if (!farm.tokenAmount || !farm.lpTotalInQuoteToken || !farm.lpTotalInQuoteToken) {
+        //   return farm
+        // }
+        const cakeRewardPerBlock = new BigNumber(farm.eggPerBlock || 1).times(new BigNumber(farm.poolWeight)) .div(new BigNumber(10).pow(18))
+        const cakeRewardPerYear = cakeRewardPerBlock.times(BLOCKS_PER_YEAR)
+
+        let apy = cakePrice.times(cakeRewardPerYear);
+
+        let totalValue = new BigNumber(farm.lpTotalInQuoteToken || 0);
+
+        if (farm.quoteTokenSymbol === QuoteToken.BNB) {
+          totalValue = totalValue.times(bnbPrice);
         }
 
-        const quoteTokenPriceUsd = prices[farm.quoteToken.symbol.toLowerCase()]
-        const totalLiquidity = new BigNumber(farm.lpTotalInQuoteToken).times(quoteTokenPriceUsd)
-        
-        const apy = isActive ? getFarmApy(farm.poolWeight, cakePrice, totalLiquidity,farm.voidPerBlock) : 0
+        if(totalValue.comparedTo(0) > 0){
+          apy = apy.div(totalValue);
+        }
 
-        return { ...farm, apy, liquidity: totalLiquidity }
+        return { ...farm, apy }
       })
-
-      if (query) {
-        const lowercaseQuery = query.toLowerCase()
-        farmsToDisplayWithAPY = farmsToDisplayWithAPY.filter((farm: FarmWithStakedValue) => {
-          return farm.lpSymbol.toLowerCase().includes(lowercaseQuery)
-        })
-      }
-      return farmsToDisplayWithAPY
+      return farmsToDisplayWithAPY.map((farm) => (
+        <FarmCard
+          key={farm.pid}
+          farm={farm}
+          removed={removed}
+          bnbPrice={bnbPrice}
+          cakePrice={cakePrice}
+          ethereum={ethereum}
+          account={account}
+        />
+      ))
     },
-    [cakePrice, prices, query, isActive],
+    [bnbPrice, account, cakePrice, ethereum],
   )
 
-  const handleChangeQuery = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setQuery(event.target.value)
-  }
-
-  let farmsStaked = []
-  if (isActive) {
-    farmsStaked = stackedOnly ? farmsList(stackedOnlyFarms) : farmsList(activeFarms)
-  } else {
-    farmsStaked = stackedOnly ? farmsList(stackedInactiveFarms) : farmsList(inactiveFarms)
-  }
-
-  farmsStaked = sortFarms(farmsStaked)
-
-  const rowData = farmsStaked.map((farm) => {
-    const { token, quoteToken} = farm
-    const tokenAddress = token.address
-    const quoteTokenAddress = quoteToken.address
-    const lpLabel = farm.lpSymbol && farm.lpSymbol.split(' ')[0].toUpperCase().replace('PANCAKE', '')
-
-    const row: RowProps = {
-      apr: {
-        value: farm.apy && farm.apy.toLocaleString('en-US', { maximumFractionDigits: 2 }),
-        multiplier: farm.multiplier,
-        lpLabel,
-        tokenAddress,
-        quoteTokenAddress,
-        cakePrice,
-        originalValue: farm.apy,
-      },
-      farm: {
-        image: farm.lpSymbol.split(' ')[0].toLocaleLowerCase(),
-        label: lpLabel,
-        pid: farm.pid,
-        depositFeeBP: farm.depositFeeBP
-      },
-      earned: {
-        earnings: farm.userData ? getBalanceNumber(new BigNumber(farm.userData.earnings)) : null,
-        pid: farm.pid,
-      },
-      liquidity: {
-        liquidity: farm.liquidity,
-      },
-      multiplier: {
-        multiplier: farm.multiplier,
-      },
-      details: farm,
-    }
-
-    return row
-  })
-
-  const renderContent = (): JSX.Element => {
-    if (viewMode === ViewMode.TABLE && rowData.length) {
-      const columnSchema = DesktopColumnSchema
-
-      const columns = columnSchema.map((column) => ({
-        id: column.id,
-        name: column.name,
-        label: column.label,
-        sort: (a: RowType<RowProps>, b: RowType<RowProps>) => {
-          switch (column.name) {
-            case 'farm':
-              return b.id - a.id
-            case 'apr':
-              if (a.original.apr.value && b.original.apr.value) {
-                return Number(a.original.apr.value) - Number(b.original.apr.value)
-              }
-
-              return 0
-            case 'earned':
-              return a.original.earned.earnings - b.original.earned.earnings
-            default:
-              return 1
-          }
-        },
-        sortable: column.sortable,
-      }))
-
-      return <Table data={rowData} columns={columns} />
-    }
-    
-    return (
+  return (
+    <Page>
+      <Heading as="h1" size="lg" color="primary" mb="50px" style={{ textAlign: 'center' }}>
+        {
+          tokenMode ?
+            TranslateString(10002, 'Stake tokens to earn EGG')
+            :
+          TranslateString(320, 'Stake LP tokens to earn EGG')
+        }
+      </Heading>
+      <Heading as="h2" color="secondary" mb="50px" style={{ textAlign: 'center' }}>
+        {TranslateString(10000, 'Deposit Fee will be used to buyback EGG')}
+      </Heading>
+      <FarmTabButtons stakedOnly={stakedOnly} setStakedOnly={setStakedOnly}/>
       <div>
+        <Divider />
         <FlexLayout>
           <Route exact path={`${path}`}>
-            {farmsStaked.map((farm) => (
-              <FarmCard key={farm.pid} farm={farm} cakePrice={cakePrice} account={account} removed={false} />
-            ))}
+            {stakedOnly ? farmsList(stakedOnlyFarms, false) : farmsList(activeFarms, false)}
           </Route>
           <Route exact path={`${path}/history`}>
-            {farmsStaked.map((farm) => (
-              <FarmCard key={farm.pid} farm={farm} cakePrice={cakePrice} account={account} removed />
-            ))}
+            {farmsList(inactiveFarms, true)}
           </Route>
         </FlexLayout>
       </div>
-    )
-  }
-
-  const handleSortOptionChange = (option: OptionProps): void => {
-    setSortOption(option.value)
-  }
-
-  return (
-    <>
-      <Header>
-        <Heading as="h1" size="xxl" color="secondary" mb="24px">
-          {isPool ? 'Pools' : 'Farms'}
-        </Heading>
-        <Heading size="lg" color="text">
-        {isPool ? TranslateString(999, 'Stake your tokens to earn VOID.') : TranslateString(999, 'Stake Liquidity Pool (LP) tokens to earn VOID.')}
-        </Heading>
-      </Header>
-      <StyledPage>
-        <ControlContainer>
-          <ViewControls>
-            <ToggleView viewMode={viewMode} onToggle={(mode: ViewMode) => setViewMode(mode)} />
-            <ToggleWrapper>
-              <Toggle checked={stackedOnly} onChange={() => setStackedOnly(!stackedOnly)} scale="sm" />
-              <Text> {TranslateString(1116, 'Staked only')}</Text>
-            </ToggleWrapper>
-            <FarmTabButtons />
-          </ViewControls>
-          <FilterContainer>
-            <LabelWrapper>
-              <Text>SORT BY</Text>
-              <Select
-                options={[
-                  {
-                    label: 'Hot',
-                    value: 'hot',
-                  },
-                  {
-                    label: 'APR',
-                    value: 'apr',
-                  },
-                  {
-                    label: 'Multiplier',
-                    value: 'multiplier',
-                  },
-                  {
-                    label: 'Earned',
-                    value: 'earned',
-                  },
-                  {
-                    label: 'Liquidity',
-                    value: 'liquidity',
-                  },
-                ]}
-                onChange={handleSortOptionChange}
-              />
-            </LabelWrapper>
-            <LabelWrapper style={{ marginLeft: 16 }}>
-              <Text>SEARCH</Text>
-              <SearchInput onChange={handleChangeQuery} value={query} />
-            </LabelWrapper>
-          </FilterContainer>
-        </ControlContainer>
-        {renderContent()}
-        {/* <StyledImage src="/images/midVoid.png" alt="mid void" width={1000} height={1000} /> */}
-      </StyledPage>
-    </>
+      <Image src="/images/egg/8.png" alt="illustration" width={1352} height={587} responsive />
+    </Page>
   )
 }
 
