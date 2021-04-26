de6694d81a98e515b4194c9c2befae0984e2f813
excavatefinance <81165639+excavatefinance@users.noreply.github.com>
2021-04-26 10:15:30 +0100

    Update locks.tsx

--- src/views/locks/locks.tsx
+++ src/views/locks/locks.tsx
@@ -1,4 +1,56 @@
+import React, { useEffect, useCallback, useState } from 'react'
+import { Route, useRouteMatch } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import BigNumber from 'bignumber.js'
+import { useWallet } from '@binance-chain/bsc-use-wallet'
+import { provider } from 'web3-core'
+import { Image, Heading } from '@pancakeswap-libs/uikit'
+import FlexLayout from 'components/layout/Flex'
+import Page from 'components/layout/Page'
 
-var lock = require('html-react-parser');
-lock('<div>text</div>');
-export default lock;
+import useRefresh from 'hooks/useRefresh'
+
+import useI18n from 'hooks/useI18n'
+
+import FarmTabButtons from './components/FarmTabButtons'
+
+export interface FarmsProps{
+  tokenMode?: boolean
+}
+
+const Locks: React.FC<FarmsProps> = (farmsProps) => {
+  const { path } = useRouteMatch()
+  const TranslateString = useI18n()
+  const { account, ethereum }: { account: string; ethereum: provider } = useWallet()
+ 
+
+  return (
+    <Page>
+      <Heading as="h1" size="lg" color="primary" mb="50px" style={{ textAlign: 'center' }}>
+        {
+          tokenMode ?
+            TranslateString(10002, 'Stake tokens to earn EGG')
+            :
+          TranslateString(320, 'Stake LP tokens to earn EGG')
+        }
+      </Heading>
+      <Heading as="h2" color="secondary" mb="50px" style={{ textAlign: 'center' }}>
+        {TranslateString(10000, 'Deposit Fee will be used to buyback EGG')}
+      </Heading>
+      <div>
+       
+        <FlexLayout>
+          <Route exact path={`${path}`}>
+            
+          </Route>
+          <Route exact path={`${path}/history`}>
+            
+          </Route>
+        </FlexLayout>
+      </div>
+      <Image src="/images/egg/8.png" alt="illustration" width={1352} height={587} responsive />
+    </Page>
+  )
+}
+
+export default Locks
